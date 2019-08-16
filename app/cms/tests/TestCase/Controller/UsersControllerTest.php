8f41d5a5e53e4549fc4bce849e85544fb1d8f32b
Kaz Watanabe <kwatanabe@heartbeats.jp>
2019-08-16 16:21:48 +0900

    UsersControllerのテストを実装

--- app/cms/tests/TestCase/Controller/UsersControllerTest.php
+++ app/cms/tests/TestCase/Controller/UsersControllerTest.php
@@ -2,8 +2,14 @@
 namespace App\Test\TestCase\Controller;
 
 use App\Controller\UsersController;
+use App\Model\Table\UsersTable;
+use Cake\Datasource\Exception\RecordNotFoundException;
 use Cake\TestSuite\IntegrationTestTrait;
+use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
+use CakeFabricate\Adaptor\CakeFabricateAdaptor;
+use Fabricate\Fabricate;
+use Fabricate\FabricateContext;
 
 /**
  * App\Controller\UsersController Test Case
@@ -15,14 +21,56 @@ class UsersControllerTest extends TestCase
     use IntegrationTestTrait;
 
     /**
+     * Test subject
+     *
+     * @var \App\Model\Table\UsersTable
+     */
+    public $Users;
+
+    /**
      * Fixtures
      *
      * @var array
      */
     public $fixtures = [
         'app.Users',
-        'app.Articles'
+        'app.Tags',
+        'app.Articles',
+        'app.ArticlesTags',
     ];
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $config = TableRegistry::getTableLocator()->exists('Users') ? [] : ['className' => UsersTable::class];
+        $this->Users = TableRegistry::getTableLocator()->get('Users', $config);
+
+        Fabricate::config(function ($config) {
+            $config->adaptor = new CakeFabricateAdaptor([CakeFabricateAdaptor::OPTION_FILTER_KEY => true]);
+        });
+
+        Fabricate::create('Users', [
+            'email' => 'foo-001@example.com',
+            'password' => 'foobarbaz',
+        ]);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Users);
+
+        parent::tearDown();
+    }
 
     /**
      * Test index method
@@ -31,7 +79,28 @@ class UsersControllerTest extends TestCase
      */
     public function testIndex()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        Fabricate::create('Users', 30, function ($data, FabricateContext $world) {
+            $email = function ($i) {
+                return sprintf("foo-%03d@example.com", $i);
+            };
+
+            return [
+                'email' => $world->sequence('email', 2, $email),
+                'password' => 'foobarbaz',
+            ];
+        });
+
+        $this->get('/users');
+        $this->assertResponseCode(200);
+        for ($i=1; $i<=20; $i++) {
+            $this->assertResponseContains(sprintf("foo-%03d@example.com", $i));
+        }
+
+        $this->get('/users?page=2');
+        $this->assertResponseCode(200);
+        for ($i=21; $i<=30; $i++) {
+            $this->assertResponseContains(sprintf("foo-%03d@example.com", $i));
+        }
     }
 
     /**
@@ -41,7 +110,12 @@ class UsersControllerTest extends TestCase
      */
     public function testView()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->get('/users/view/1');
+        $this->assertResponseCode(200);
+        $this->assertResponseContains('foo-001@example.com');
+
+        $this->get('/tags/view/2');
+        $this->assertResponseCode(404);
     }
 
     /**
@@ -51,7 +125,22 @@ class UsersControllerTest extends TestCase
      */
     public function testAdd()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $data = [
+            'email' => 'foo@example.com',
+            'password' => 'foobarbaz',
+        ];
+
+        $this->enableCsrfToken();
+        $this->get('/users/add');
+        $this->assertResponseCode(200);
+
+        $this->enableCsrfToken();
+        $this->post('/users/add', $data);
+        $this->assertResponseCode(302);
+        $this->assertRedirect('/users');
+
+        $result = $this->Users->get(2);
+        $this->assertEquals('foo@example.com', $result->email);
     }
 
     /**
@@ -61,7 +150,28 @@ class UsersControllerTest extends TestCase
      */
     public function testEdit()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $data = [
+            'email' => 'foo@example.com',
+        ];
+
+        $this->enableCsrfToken();
+        $this->get('/users/edit/1');
+        $this->assertResponseCode(302);
+        $this->assertRedirectContains('/users/login');
+
+        $this->enableCsrfToken();
+        $this->put('/users/edit/1', $data);
+        $this->assertResponseCode(302);
+        $this->assertRedirect('/users/login');
+
+        $this->session(['Auth' => ['User' => ['id' => 1]]]);
+        $this->enableCsrfToken();
+        $this->put('/users/edit/1', $data);
+        $this->assertResponseCode(302);
+        $this->assertRedirect('/users');
+
+        $result = $this->Users->get(1);
+        $this->assertEquals('foo@example.com', $result->email);
     }
 
     /**
@@ -71,6 +181,49 @@ class UsersControllerTest extends TestCase
      */
     public function testDelete()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->enableCsrfToken();
+        $this->post('/users/delete/1');
+        $this->assertResponseCode(302);
+        $this->assertRedirectContains('/users/login');
+
+        $this->session(['Auth' => ['User' => ['id' => 1]]]);
+        $this->enableCsrfToken();
+        $this->post('/users/delete/1');
+        $this->assertResponseCode(302);
+        $this->assertRedirect('/users');
+
+        $this->expectException(RecordNotFoundException::class);
+        $this->Users->get(1);
+    }
+
+    public function testLogin()
+    {
+        $this->enableCsrfToken();
+        $this->get('/users/login');
+        $this->assertResponseCode(200);
+
+        $data = [
+            'email' => 'foo-001@example.com',
+            'password' => '_error_password_',
+        ];
+        $this->post('/users/login', $data);
+        $this->assertResponseCode(200);
+        $this->assertResponseContains('Your username or password is incorrect.');
+
+        $data = [
+            'email' => 'foo-001@example.com',
+            'password' => 'foobarbaz',
+        ];
+        $this->post('/users/login', $data);
+        $this->assertResponseCode(302);
+        $this->assertRedirect('/');
+    }
+
+    public function testLogout()
+    {
+        $this->enableCsrfToken();
+        $this->get('/users/logout');
+        $this->assertResponseCode(302);
+        $this->assertRedirectContains('/users/login');
     }
 }
